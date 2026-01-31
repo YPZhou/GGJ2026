77baf36beafa9c6dc15ab6649c86c59839a415f6
Cataphract <44166626+cAtAphrACtCtl@users.noreply.github.com>
2026-01-31 11:32:11 +0900

    tv image

--- Game/Script/TV.cs
+++ Game/Script/TV.cs
@@ -6,14 +6,70 @@ public enum TVStatus
 	BAD, // 正在播放坏图片
 }
 
-public partial class TV : Node
+public partial class TV : Sprite2D
 {
 	public static TVStatus CurrentStatus { get; set; }
+
+	[Export]
+	Game game;
+	[Export]
+	Texture2D goodImage;
+	[Export]
+	Texture2D badImage;
+	[Export]
+	Texture2D mosaicImage;
 
 	public override void _Ready()
 	{
 		base._Ready();
 
 		CurrentStatus = TVStatus.BAD;
+
+		CheckRes();
+
+		SetImage(TVStatus.GOOD);
 	}
+
+	void CheckRes()
+	{
+		if (game == null)
+		{
+			GD.PrintErr("Game is null, set Game for TV.game in the editor.");
+		}
+		if (goodImage == null)
+		{
+			GD.PrintErr("Good image is null, set good image for TV.goodImage in the editor.");
+		}
+		if (badImage == null)
+		{
+			GD.PrintErr("Bad image is null, set bad image for TV.badImage in the editor.");
+		}
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		
+	}
+
+	void SetImage(TVStatus status)
+	{
+		if (status == CurrentStatus)
+		{
+			return;
+		}
+
+		CurrentStatus = status;
+		switch (status)
+		{
+			case TVStatus.GOOD:
+				Texture = goodImage;
+				break;
+			case TVStatus.BAD:
+				Texture = badImage;
+				break;
+		}
+	}
+
 }
