c7e4858969b519152bf2e0a01296cf2db972810f
Cataphract <44166626+cAtAphrACtCtl@users.noreply.github.com>
2026-02-01 01:32:02 +0900

    gizmo for visual debug

---
+++ Game/Script/Gizmo.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+public partial class Gizmo : Node2D
+{
+    Vector2[] points;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        ZIndex = 1000;
+    }
+    public override void _Draw()
+    {
+        base._Draw();
+
+        if (points != null)
+        {
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                DrawLine(points[i], points[i + 1], Colors.Red, 2);
+            }
+        }
+    }
+
+    public void SetPoints(Vector2[] points)
+    {
+        this.points = points;
+    }
+
+}
