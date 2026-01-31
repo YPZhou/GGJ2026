d59e42562bb7e445a1d7b222a89fcee028513429
YPZhou <yunpeng_zhou@163.com>
2026-01-31 08:17:22 +0800

    时间结束时跳转结果场景

--- Game/Script/Game.cs
+++ Game/Script/Game.cs
@@ -2,14 +2,41 @@ using Godot;
 
 public partial class Game : Node
 {
+	[Export] PackedScene resultScene;
+
 	[Export] Cat cat;
 	[Export] Hand hand;
 	[Export] TV tv;
 
 	[Export] int totalTime; // 单局时长，30秒
+	double elapsedTime = 0;
+
+	bool IsGameEnd => elapsedTime >= totalTime;
 
 	public override void _Ready()
 	{
 		base._Ready();
+		elapsedTime = 0;
+	}
+
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+
+		if (!IsGameEnd)
+		{
+			elapsedTime += delta;
+			if (IsGameEnd)
+			{
+				if (resultScene != null)
+				{
+					GetTree().ChangeSceneToPacked(resultScene);
+				}
+				else
+				{
+					GD.PrintErr("Result scene not set in Game.resultScene");
+				}
+			}
+		}
 	}
 }
